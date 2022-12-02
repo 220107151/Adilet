d84dd541a13a3a17f318a13649223a8806d538ed
Iliyas <220107151@stu.sdu.edu.kz>
2022-12-02 19:28:24 +0600

    Nartay Ilyas

---
+++ src/Questions.java
@@ -0,0 +1,11 @@
+public class Questions {
+    public static void main(String[] args) {
+        System.out.println("Name: Iliyas");
+        System.out.println("Surname: Nartay");
+        System.out.println("Id: 220107151");
+        System.out.println("Hobby: anime");
+        System.out.println("How much do you like ICT(from 1-10)): 10");
+        System.out.println("Rate this task from 1 to 10: 10");
+        System.out.println("Am I the best teacher?: of course");
+    }
+}
