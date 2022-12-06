b45526f92a197d2eb498868fd84650fbe83fd98a
Your Name <220107098@stu.sdu.edu.kz>
2022-12-06 12:14:06 +0600

    first commit

--- src/Questions.java
+++ src/Questions.java
@@ -1,9 +1,9 @@
-public class Modify_Questions {
+public class Questions {
     public static void main(String[] args) {
         System.out.println("Name: Adilet");
         System.out.println("Surname: Toikenov");
         System.out.println("Id: 220107098");
-        System.out.println("Hobby: anime");
+        System.out.println("Hobby: video games");
         System.out.println("How much do you like ICT(from 1-10)): 10");
         System.out.println("Rate this task from 1 to 10: 10");
         System.out.println("Am I the best teacher?: YES");
